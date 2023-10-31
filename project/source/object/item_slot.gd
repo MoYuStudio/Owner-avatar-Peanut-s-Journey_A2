4e427e147d3f72196606c730012b22fc135e7879
WilsonVinson <cprewilsonvinson@gmail.com>
2023-10-31 11:59:20 +0800

    main

--- project/source/object/item_slot.gd
+++ project/source/object/item_slot.gd
@@ -6,7 +6,7 @@ func _get_drag_data(at_position):
 		var texture = TextureRect.new()
 		texture.texture = $Sprite.texture
 		texture.scale = $Sprite.scale
-		texture.TEXTURE_FILTER_NEAREST
+		texture.filter = texture.TEXTURE_FILTER_NEAREST
 		set_drag_preview(texture)
 		return $Sprite
 	return true
